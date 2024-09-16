e4235df71f7a47c41d327c7c8217f653013b03df
Kau17478 <kau17478@sheridancollege.ca>
2024-09-16 13:15:47 -0400

    Update Softwaredesign_1.java

--- src/softwaredesign_1/Softwaredesign_1.java
+++ src/softwaredesign_1/Softwaredesign_1.java
@@ -14,6 +14,7 @@ public class Softwaredesign_1 {
      * @param args the command line arguments
      */
     // added a comment
+    // showing pull
     public static void main(String[] args) {
         System.out.println("Hello");
         // TODO code application logic here
